bc262d361d9daf12729348e889dc25010e98b671
Kishan-Panday <kishanpandey4463@gmail.com>
2024-12-06 21:09:55 +0530

    forloop

---
+++ dowhile.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class dowhile {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int i=0;
+        while(i<11){
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}
--- firstclass.java
+++ firstclass.java
@@ -11,4 +11,4 @@ public static void main( String[] args) {
 
 }
 } 
-                            
+                           
