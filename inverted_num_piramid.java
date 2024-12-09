481fe65c7b53e48c27727a6c5d30cf7f88eebff6
Kishan-Panday <kishanpandey4463@gmail.com>
2024-12-09 22:36:24 +0530

    inverted_half_piramid_of_num

--- inverted_num_piramid.java
+++ inverted_num_piramid.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
-public class piramidofnum {
-    public static void main(String[] args) {
+public class inverted_num_piramid {
+     public static void main(String[] args) {
         Scanner sc =  new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        for(int i=1;i<n;i++){
+        for(int i=n;i>0;i--){
             for(int j=1;j<=i;j++){
                 System.out.print(j+" ");
             }
