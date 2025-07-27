5a8ad58467b04803a53a6ee4f442c8fe9220c2d3
Kishan-Panday <kishanpandey4463@gmail.com>
2025-07-27 18:42:47 +0530

    Pattern printing num in triangle

--- pattern.java
+++ pattern.java
@@ -6,7 +6,7 @@ public class pattern {
         int a= sc.nextInt();
         for(int i =1;i<=a;i++){
             for(int j=1;j<=i;j++){
-                System.out.print(j);
+                System.out.print(j+" ");
             }
             System.out.println();
         }
