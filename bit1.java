d81185969260910ef90b78e98ae764fa2d68bca8
Kishan-Panday <kishanpandey4463@gmail.com>
2025-07-31 23:10:31 +0530

    Bit manipualtion

---
+++ bit1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class bit1 {
+    public static void main(String[] args) {
+        int n= 5;
+        int pos=3;
+        int bitMask = 1<<pos;
+         //get Bit
+        if((bitMask & n)==0){
+            System.out.println("Bit is Zero");
+        }else
+         System.out.println("Bit is One");
+        //set Bit
+        int newNumber= bitMask | n;
+        System.out.println(newNumber);
+       
+         //clear Bit
+         int x=~bitMask;
+            System.out.println(x&n);
+            //update Bit
+            //oper->1 :set   oper->0 :clear
+            System.out.println("Enter the value in term 1 0r 0:");
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            if(a==1){
+                 
+                System.out.println(newNumber);
+            }else{
+            
+            System.out.println(x&n);
+            }
+         }
+    }
+    
+
