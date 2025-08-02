a55468a6499418f725e6eea74a28e27ded68e3e1
Kishan-Panday <kishanpandey4463@gmail.com>
2025-08-02 22:13:55 +0530

    print arr

--- print.java
+++ print.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 public class print{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int arr[]= new int[5];
+        String arr[]= new String[5];
         for(int i=0;i<arr.length; i++){
-            arr[i]= sc.nextInt();
+            arr[i]= sc.next();
         }
         System.out.println(Arrays.toString(arr));
     }
