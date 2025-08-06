50528c7c24b985a7a4e309ebcbffcdad97c53ba6
Kishan-Panday <kishanpandey4463@gmail.com>
2025-08-06 17:52:45 +0530

    pattern printing

--- pattern_4.java
+++ pattern_4.java
@@ -1,7 +1,5 @@
-import java.util.Scanner;
-
-public class pattern_1 {
-    public static void main(String[] args) {
+public class pattern_4 {
+     public static void main(String[] args) {
         for(int i=1;i<=5;i++){
             for(int j=1;j<=i;j++){
                 System.out.print("* ");
@@ -9,10 +7,6 @@ public class pattern_1 {
             }
             System.out.println();
         }
-        // for(int j=1;j<=5;j++){
-        //         System.out.print("* ");}
-        //         System.out.println();
-
             for(int i=1;i<=4;i++){
             for(int j=1;j<=5-i;j++){
                 System.out.print("* ");
