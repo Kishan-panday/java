3c325f10a1a5f80b97ef74e78252ea0f07da2638
Kishan-Panday <kishanpandey4463@gmail.com>
2025-08-08 19:59:26 +0530

    linear search in string

--- Linearse.java
+++ Linearse.java
@@ -2,18 +2,18 @@ public class Linearse {
 
      public static int search(int target,int arr[]){
         if(arr.length==0) return -1;
-        for(int i=0;i<arr.length;i++){
-            int element=arr[i];
+        for(int index=0;index<arr.length;index++){
+            int element=arr[index];
             if(element==target){
-                return i;
+                return index;
             }
         }
         return -1;
     }
     public static void main(String[] args) {
-        int arr[]={2,3,4,5};
-        int target= 4;
-      int ans=  search(target,arr);
+        int num[]={2,3,4,5};
+        int a= 4;
+      int ans=  search(a,num);
       System.out.println(ans);
     }
 }
