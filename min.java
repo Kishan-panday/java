444b8dd5f5adabaf318ae01c745fdadb58fd6b09
Kishan-Panday <kishanpandey4463@gmail.com>
2025-08-09 14:16:54 +0530

    find min

---
+++ min.java
@@ -0,0 +1,15 @@
+public class min {
+    public static void main(String[] args) {
+        int arr [] ={2,3,-3,5,-6};
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<max) {
+                max = arr[i];
+            }
+            
+            }
+            System.out.println(max);
+
+        }
+    }
+
