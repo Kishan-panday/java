865b27d49c87745c14f8b15fafa1656d8325f4cc
Kishan-Panday <kishanpandey4463@gmail.com>
2025-08-15 23:29:58 +0530

    Bubble sorting

--- search2d.java
+++ search2d.java
@@ -9,13 +9,13 @@ public class search2d {
             {13,14,15,16}
 
         };
-        System.out.println(Arrays.toString(search(arr, 15)));
+        System.out.println(Arrays.toString(search(arr, 13)));
         
     }
     static int[] search(int[][] arr ,int target){
         int r=0;
         int c = arr.length-1;
-        while(r<arr.length && c>=0){
+        while(r<=arr.length-1 && c>=0){
             if(arr[r][c]==target){
                 return new int[] {r,c};
             }
