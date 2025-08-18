876a4a6608b911a569ca3d0bdb89df3d389a79ff
Kishan-Panday <kishanpandey4463@gmail.com>
2025-08-18 21:40:22 +0530

    Bubble sort

---
+++ soeting/bubblesort.java
@@ -0,0 +1,25 @@
+package soeting;
+   import java.util.Arrays;
+public class bubblesort {
+    public static void main(String[] args) {
+        int arr[] = {8,7,6,9,45,3,267,566};
+System.out.println(Arrays.toString(sort(arr)));
+
+    }
+    static int[] sort(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=1;j<arr.length-i;j++){
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j]= arr[j-1];
+                    arr[j-1] = temp; 
+                }
+            }
+        }
+    
+        return arr;
+    }
+}
+
+    
+
